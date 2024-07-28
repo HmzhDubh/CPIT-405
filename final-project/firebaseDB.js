21fe4e05717b9c43ba976a789e165fa0f1b8306b
hmzhdubh <meezo.2015@hotmail.com>
2024-07-28 09:35:15 +0300

    adding db

---
+++ final-project/firebaseDB.js
@@ -0,0 +1,25 @@
+// // Your web app's Firebase configuration
+//const firebaseConfig = {
+//apiKey: "AIzaSyAIctYvepVDKAyZrUpAc76qEkTfx8WaNBQ",
+//authDomain: "m3h-restaurant.firebaseapp.com",
+//databaseURL: "https://m3h-restaurant-default-rtdb.firebaseio.com",
+//projectId: "m3h-restaurant",
+//storageBucket: "m3h-restaurant.appspot.com",
+//messagingSenderId: "164803237196",
+//appId: "1:164803237196:web:5c6bdaf13c585ced216be9"
+//};
+//// init the firebase DB
+//firebase.initializeApp(firebaseConfig);
+//// reference the DB
+// var M3H-DB = firebase.database().ref('M3H-restaurantForm');
+//
+//document.getElementById("projForm").addEventListener("submit", submitForm)
+//
+//function submitForm(e){
+//    e.preventDefault();
+//    var name = getElementValue('fname');
+//    console.log(name);
+//}
+//const getElementValue = (id) => {
+//    return document.getElementById(id).value;
+//}
--- final-project/theProject-script.js
+++ final-project/theProject-script.js
@@ -1,6 +1,7 @@
 let menu = document.querySelector('#menu-bar');
 let navbar = document.querySelector('.navigator');
 
+
 menu.onclick = () =>{
 
   menu.classList.toggle('fa-times');
@@ -62,6 +63,75 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
+ // Your web app's Firebase configuration
+const firebaseConfig = {
+apiKey: "AIzaSyAIctYvepVDKAyZrUpAc76qEkTfx8WaNBQ",
+authDomain: "m3h-restaurant.firebaseapp.com",
+databaseURL: "https://m3h-restaurant-default-rtdb.firebaseio.com",
+projectId: "m3h-restaurant",
+storageBucket: "m3h-restaurant.appspot.com",
+messagingSenderId: "164803237196",
+appId: "1:164803237196:web:5c6bdaf13c585ced216be9"
+};
+
+// init the firebase DB
+firebase.initializeApp(firebaseConfig);
+
+// reference the DB
+var formDB = firebase.database().ref('M3H-restaurantForm');
+
+document.getElementById("projForm").addEventListener("submit", submitForm)
+
+function submitForm(e){
+    e.preventDefault();
+    var firstName = document.getElementById('fname').value;
+    var lastName = document.getElementById('lname').value;
+    var phone = document.getElementById('phone').value;
+    var email = document.getElementById('email').value;
+    var address = document.getElementById('address').value;
+    var priceString = document.getElementById('outputPrice').innerText;
+    var price = priceString.substring(12, 15);
+    if(firstName == "" || lastName == "" || phone == ""|| email == ""|| address == ""|| price==""){
+        const alert = document.getElementById('msg');
+        alert.style.display = "block";
+        // show error msg
+        let text = 'Please Enter All data correctly';
+        alert.innerText = text;
+        //remove error msg
+        setTimeout(() => {
+            alert.style.display = "none";
+        }, 3000);
+
+    }
+    else{
+     saveData(firstName, lastName, phone, email, address, price);
+     // show the Alert message
+      const alert = document.querySelector('.alert');
+      alert.style.display = "block";
+      let text = 'Your order is successfully submitted ✅';
+      alert.innerText = text;
+
+      // Remove the message
+      setTimeout(() => {
+        alert.style.display = "none";
+      }, 3000);
+      // reset the form
+      document.getElementById("projForm").reset();
+    }
+}
+
+    const saveData = (firstName, lastName, phone, email, address, price) => {
+        var newFormData = formDB.push();
+        newFormData.set({
+            fname: firstName,
+            lname: lastName,
+            phone: phone,
+            email: email,
+            address: address,
+            price: price,
+        });
+    }
+
     const resetButton = document.getElementById('re');
     if (resetButton) {
         resetButton.addEventListener('click', function(e) {
@@ -69,6 +139,8 @@ document.addEventListener('DOMContentLoaded', function() {
             console.log("Reset button clicked");
             currentValue = 0;
             outputPrice.textContent = "Total price: " + currentValue + " SR";
+            const alert = document.querySelector('.alert');
+            alert.style.display = "none";
         });
     } else {
         console.log("Reset button not found");

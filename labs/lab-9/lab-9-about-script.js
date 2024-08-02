e780a46a9bb390ae2f11b84f5019b6cc4f0b0e3a
hmzhdubh <meezo.2015@hotmail.com>
2024-08-02 09:56:50 +0300

    fix lab 9  and add chapter 4

---
+++ labs/lab-9/lab-9-about-script.js
@@ -0,0 +1,49 @@
+
+(function () {
+ const url =  window.location.href
+
+ let idFromUrl = url.substring(58, url.length);
+ let newSection = document.createElement("p");
+ newSection.innerText = url;
+
+ let he = document.createElement("h1");
+ he.innerText = idFromUrl;
+
+ const v = document.getElementById("PageInfo");
+ v.appendChild(newSection);
+ v.appendChild(he);
+
+ getRecipeById(idFromUrl);
+})();
+
+function getRecipeById(id){
+
+    const APIKEY = "your_api_key";
+    console.log(":"+id+";");
+    var xhr2 = new XMLHttpRequest();
+    xhr2.onreadystatechange = function() {
+        if (xhr2.readyState === 4 && xhr2.status === 200) {
+            const data2 = JSON.parse(xhr2.responseText);
+            createRecipe(data2);
+        } else if (xhr2.readyState === 4 && xhr2.status != 200) {
+            console.error('Error:', xhr2.status, xhr2.statusText);
+
+        }
+    }
+}
+    xhr2.open('GET', `https://api.spoonacular.com/recipes/${id}/information?apiKey=${APIKEY}`, true); // true for async request
+    xhr2.send();
+}
+
+function createRecipe(data2){
+    for (item of data2[0]) {
+
+        let newDiv = document.createElement("div");
+        let par = document.createElement("p");
+        par.innerText = data2.join();
+        newDiv.appendChild(par);
+
+        const divInfo = document.getElementById("PageInfo");
+        divInfo.appendChild(newDiv);
+    }
+}
---
+++ labs/lab-9/lab-9-home-script.js
@@ -0,0 +1,50 @@
+const searchResults = document.getElementById("searchResults");
+// Put Your API Key that you created in spoonacular Dashboard
+const APIKEY = "your_api_key";
+
+// send an HTTP request using XMLHttpRequest
+function searchUsingXHR() {
+    let keyword = document.getElementById("keyword").value;
+    searchResults.innerHTML = "";
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const data = JSON.parse(xhr.responseText);
+            createDOMElements(data);
+            console.log(data);
+        } else if (xhr.readyState === 4 && xhr.status != 200) {
+
+            console.error('Error:', xhr.status, xhr.statusText);
+        }
+    }
+    xhr.open('GET', `https://api.spoonacular.com/recipes/complexSearch?query=${keyword}&apiKey=${APIKEY}`, true); // true for async request
+    xhr.send();
+}
+
+// create DOM elements based on the response data
+function createDOMElements(data) {
+    for (item of data.results) {
+
+        if (item.imageType === "jpg") {
+
+            let imgDiv = document.createElement("div");
+            let imgElem = document.createElement("img");
+            let imgA = document.createElement("a");
+
+
+            imgDiv.class = "recipe-item";
+            imgElem.src = item.image;
+            var itemId = item.id;
+            imgA.innerText = item.title;
+            imgA.href = `./lab-9-about.html?rId=${itemId}`;
+            imgA.id = itemId;
+
+            imgDiv.appendChild(imgElem);
+            imgDiv.appendChild(imgA);
+            searchResults.appendChild(imgDiv);
+
+        } else {
+            alert("Type Not supported yet");
+        }
+    }
+}

ac0ad6e3979d99ae9db410a24e9eb9e35052766f
HmzhDubh <hhussainduba@stu.kau.edu.sa>
2024-10-01 09:59:57 +0300

    edit copyrights

--- labs/lab-9/lab-9-about-script.js
+++ labs/lab-9/lab-9-about-script.js
@@ -7,7 +7,7 @@
 
 function getRecipeById(id){
 
-    const APIKEY = "47d8f549f16143e39ae9fc5106692b69";
+    const APIKEY = "your_api_key";
     console.log(">>>"+id+";");
     var xhr2 = new XMLHttpRequest();
     xhr2.onreadystatechange = function() {
--- labs/lab-9/lab-9-home-script.js
+++ labs/lab-9/lab-9-home-script.js
@@ -1,6 +1,6 @@
 const searchResults = document.getElementById("searchResults");
 // Put Your API Key that you created in spoonacular Dashboard
-const APIKEY = "47d8f549f16143e39ae9fc5106692b69";
+const APIKEY = "your_api_key";
 
 // send an HTTP request using XMLHttpRequest
 function searchUsingXHR() {
